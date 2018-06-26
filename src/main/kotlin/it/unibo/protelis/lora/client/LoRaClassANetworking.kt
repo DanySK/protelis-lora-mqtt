37acd42f66a97c40a08cc13e7549c8fd0dfea7d1
Danilo Pianini <danilo.pianini@unibo.it>
2018-06-26 18:21:04 +0200

    Complete client side

--- src/main/kotlin/it/unibo/protelis/lora/client/LoRaClassANetworking.kt
+++ src/main/kotlin/it/unibo/protelis/lora/client/LoRaClassANetworking.kt
@@ -1,26 +1,48 @@
 package it.unibo.protelis.lora.client
 
+import it.unibo.protelis.lora.DownlinkSerializer
+import it.unibo.protelis.lora.HexString
 import it.unibo.protelis.lora.Segmenter
-import org.apache.commons.codec.binary.Hex
+import it.unibo.protelis.lora.ThreeBytesSegmenter
+import it.unibo.protelis.lora.TwoBytesSizedHeader
 import org.protelis.lang.datatype.DeviceUID
 
-class LoRaClassANetworking(protected val node: RN2483, protected val segmenter: Segmenter) {
+class LoRaClassANetworking(
+    private val node: RN2483,
+    private val segmenter: Segmenter = ThreeBytesSegmenter,
+    private val downlinkSerializer: DownlinkSerializer = TwoBytesSizedHeader) {
 
     private var transaction: Byte = 0
 
-    fun send(payload: ByteArray): Map<DeviceUID, ByteArray> {
-        var received: ByteArray = ByteArray(0)
+    fun send(payload: ByteArray): Map<out DeviceUID, ByteArray> {
+        var received: List<ByteArray> = emptyList()
         segmenter.segment(payload, transactionId = transaction++).forEach {
-            val hex = Hex.encodeHexString(it)
-            println("Sending $hex")
-            val response = node.execute(Command.Mac.Transmit(node, hex))
-            when (response) {
-                is Response.Ok -> when(response.then) {
-//                    is Response.DataReceived -> received += response.then.data
+            val response = sendAndReceive(HexString(it))
+            response?.also {
+                if (segmenter.transactionComplete(received)) {
+                    throw IllegalStateException("Unexpected response: $it, transaction was complete.")
                 }
+                received += it
             }
         }
-        TODO()
+        while (!segmenter.transactionComplete(received)) {
+            received += sendAndReceive(HexString("")) ?: throw IllegalStateException("Missing response data")
+        }
+        return downlinkSerializer.deserialize(HexString(segmenter.assemble(received)))
+    }
+
+    private fun sendAndReceive(payload: HexString): ByteArray? {
+        println("Sending ${payload}")
+        val response = node.execute(Command.Mac.Transmit(node, payload))
+        return when (response) {
+            is Response.Ok -> response.then ?.let {
+                when(it) {
+                    is Response.DataReceived -> HexString(it.data).toByteArray()
+                    else -> throw IllegalStateException("Unexpected response: $response")
+                }
+            }
+            else -> throw IllegalStateException("Unexpected response: $response")
+        }
     }
 
 }
